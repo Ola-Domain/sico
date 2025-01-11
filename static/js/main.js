4b51d37494a8e5eb065dc7c967541bcd167d94f8
Ola-Domain <olasupooladotun6@gmail.com>
2025-01-11 21:40:23 +0000

    new

--- static/js/main.js
+++
@@ -1,7 +0,0 @@
-// JavaScript for swiping videos and handling actions
-console.log("PikOne JS initialized!");
-
-// Example: Handling Like button clicks
-document.querySelectorAll(".like-btn").forEach(btn => {
-  btn.addEventListener("click", () => alert("Video liked!"));
-});
