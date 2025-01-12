8478b80cb65fef23174667f1cc94e1d0c81945b5
Ola-Domain <olasupooladotun6@gmail.com>
2025-01-12 14:48:07 +0000

    new

--- templates/SSL.php
+++
@@ -1,1 +0,0 @@
-$conn = new mysqli($sico.mysql.database.azure.com, $sico, $Ola-Mide&2, $sico, 3306, '/path/to/DigiCertGlobalRootCA.crt.pem');
--- templates/submit.php
+++
@@ -1,7 +0,0 @@
-$sql = "INSERT INTO videos (name, email, video_path) VALUES ('$name', '$email', '$target_file')";
-if ($conn->query($sql) === TRUE) {
-    echo "Record saved successfully.";
-} else {
-    echo "Error: " . $conn->error;
-}
-$conn->close();
